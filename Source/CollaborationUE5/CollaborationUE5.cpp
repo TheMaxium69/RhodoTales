d51ee947abad4c00f0ac7343d55314ca9e38734f
Maxime Tournier <mxm.tournier@gmail.com>
2024-03-06 22:16:34 +0100

    Init UE5

---
+++ Source/CollaborationUE5/CollaborationUE5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CollaborationUE5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CollaborationUE5, "CollaborationUE5" );
